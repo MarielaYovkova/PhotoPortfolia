6ed00f4ab843323258a3e5b6aef643789be4fbcc
MarielaYovkova <122797627+MarielaYovkova@users.noreply.github.com>
2026-03-06 09:54:11 +0200

    PhotosController added

---
+++ PhotoPortfolia/Controllers/PhotosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhotoPortfolia.Models;
+
+public class PhotosController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public PhotosController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> AlbumPhotos(int albumId)
+    {
+        var album = await _context.Albums
+            .Include(a => a.Photos)
+            .FirstOrDefaultAsync(a => a.Id == albumId);
+
+        if (album == null) return NotFound();
+
+        return View(album);
+    }
+}
