b473699484a2a070ebd16293afe68891c9b9fd85
MarielaYovkova <122797627+MarielaYovkova@users.noreply.github.com>
2026-04-13 21:31:32 +0300

    Implemented CategoryService unit tests using In-Memory database

---
+++ PhotoPortfolia.Tests/Services/CategoryServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhotoPortfolia.Data;
+using PhotoPortfolia.Services;
+using PhotoPortfolia.ViewModels;
+using Xunit;
+
+namespace PhotoPortfolia.Tests.Services
+{
+    public class CategoryServiceTests
+    {
+        private DbContextOptions<ApplicationDbContext> _options;
+        private ApplicationDbContext _context;
+        private CategoryService _service;
+
+        public CategoryServiceTests()
+        {
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new ApplicationDbContext(_options);
+            _service = new CategoryService(_context);
+        }
+
+        [Fact]
+        public async Task CreateCategoryAsync_ShouldAddCategoryCorrectly()
+        {
+            // Arrange
+            var model = new CategoryViewModel { Name = "Nature" };
+
+            // Act
+            await _service.CreateCategoryAsync(model);
+
+            // Assert
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Name == "Nature");
+            Assert.NotNull(category);
+            Assert.Equal("Nature", category.Name);
+        }
+    }
+}
---
+++ PhotoPortfolia.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PhotoPortfolia.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
